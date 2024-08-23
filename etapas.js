7c930188e3acc2da1486418776c8eb78fda378a3
Pascoal Fernandes <147885199+Lost-Fernandes@users.noreply.github.com>
2024-08-23 08:11:14 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,36 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '15999',
+                nome: 'Dr. RONEY',
+                partido: 'MDB',
+                fotos:[
+                    {url:'dr.rondiney.jfif', legenda: 'Vereador'}
+                ]
+            },
+            
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'TADEU DIAS',
+                partido: 'PT',
+                vice: 'AMAURI BRAGA',
+                fotos:[
+                    {url:'prefeito.jfif', legenda: 'Prefeito'},
+                    {url:'viceprefeito.jfif', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            
+        ]
+    }
+];
+
+
